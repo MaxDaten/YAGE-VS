870806cb9d40fca9ae54d6c9229cec905d975f13
Jan-Philip Loos <maxdaten@gmail.com>
2011-09-29 01:05:04 +0200

    added SimpleTest Project and moved Props file
    + added a SimpleTest Project for the active Lib testing
    ~ moved and renamed 'Additinal Directory Macros.probs' to YAGE.props. Its
    now a general Solution Props File with some nice conditional suffixing and
    file copying

---
+++ src/SimpleTest/SimpleTest.cpp
@@ -0,0 +1,18 @@
+#include "SimpleTest.h"
+
+
+SimpleTest::SimpleTest(void)
+{
+}
+
+
+SimpleTest::~SimpleTest(void)
+{
+}
+
+
+int main(int argc, char** argv)
+{
+	SimpleTest App;
+	return App.Main();
+}
---
+++ src/SimpleTest/SimpleTest.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "SFMLApplication.h"
+
+class SimpleTest :
+	public SFMLApplication
+{
+public:
+	SimpleTest(void);
+	~SimpleTest(void);
+};
+
